55b8a5cddcec629965c48a59b095145ca5577e1f
Yavor Georgiev <jimbolp@gmail.com>
2017-11-29 13:28:40 +0200

    BaseViewModel ExpandCommand

---
+++ WpfTreeView/Directory/VewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Base View Model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event to be fired
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
--- WpfTreeView/Directory/VewModels/DirectoryItemViewModel.cs
+++ WpfTreeView/Directory/VewModels/DirectoryItemViewModel.cs
@@ -1,12 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WpfTreeView
 {
-    public class DirectoryItemViewModel
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DirectoryItemViewModel : BaseViewModel
     {
+        #region Public Properties
+        /// <summary>
+        /// The type of the directory item. Either Drive, folder or file
+        /// </summary>
+        public DirectoryItemType Type { get; set; }
+        
+        /// <summary>
+        /// The full path to the file/folder
+        /// </summary>
+        public string FullPath { get; set; }
+        
+        /// <summary>
+        /// The name of this directory item
+        /// </summary>
+        public string Name { get { return Type == DirectoryItemType.Drive ? FullPath : DirectoryStructure.GetFileFolderName(this.FullPath); } }
+
+        /// <summary>
+        /// A list of all children
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Children { get; set; }
+
+        /// <summary>
+        /// Indicates if this item if file or not. If it is a file it cannot be expanded (so it returns "False")
+        /// </summary>
+        public bool CanExpand { get { return this.Type != DirectoryItemType.File; } }
+
+        public bool IsExpanded
+        {
+            get
+            {
+                return Children?.Count(f => f != null) > 0;
+            }
+            set
+            {
+                //The UI tells us to expand
+                if(value == true)
+                {
+                    Expand();
+                }
+                //We have to collapse
+                else
+                {
+                    ClearChildren();
+                }
+
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        /// <summary>
+        /// Removes the children from the list
+        /// </summary>
+        private void ClearChildren()
+        {
+            Children = new ObservableCollection<DirectoryItemViewModel>();
+
+            if(Type != DirectoryItemType.File)
+            {
+                Children.Add(null);
+            }
+        }
+        #endregion
+
+        #region Public Commands
+
+        public ICommand ExpandCommand { get; set; }
+
+        #endregion
+        /// <summary>
+        /// Expand the directory and find the cildren
+        /// </summary>
+        private void Expand()
+        {
+
+        }
     }
 }
